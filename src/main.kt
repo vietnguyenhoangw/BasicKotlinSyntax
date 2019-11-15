70d4bed3408053400b548129796b71345b6d86ec
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2019-11-15 18:06:00 +0700

    [UPD] - :pencil: part 2 (null safety)

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,12 @@
-fun main(args: Array<String>) {
+fun main() {
     // call function name here:
-    array_datatype()
+    nonnull_type()
 }
+
+/*
+* part 1
+* data type, var, array
+* */
 
 fun var_and_val() {
     var name = "daniel"
@@ -148,4 +153,42 @@ fun comment() {
     * */
 
     // single line comment
+}
+
+/**
+ * part 2
+ * null safety
+ * docs: https://cungdev.com/kiem-tra-bien-null-kotlin/
+ */
+
+fun nonnull_type() {
+    var name : String = "daniel"
+
+    // name = null -> var's data type in part 1 is not null value
+
+    var name2 : String? = null
+
+    // add '?' symbol if you want var is null
+
+    /* safe call
+    * */
+    var name3 : String? = "hello world"
+    var lengthOfname = name3?.length
+
+    println("length: $lengthOfname")
+
+    // another case to check var null
+    // name2 = null ? skip : excute the command
+    name2?.let {
+        var length = name2.length
+        println("name2.length = $length")
+    }
+
+    // If the expression is on the left '?:' symbol is not null
+    //       length's value will be name3.length
+    // else
+    //       length's value = -1
+    val length = name3?.length ?: -1
+
+    println("last length: $length")
 }
